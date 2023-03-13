aafff655096806e8c57b2cbb3d55793c00bb9d3a
Juan Valencia <johnjuan15@gmail.com>
2023-03-13 14:58:26 -0700

    calclab

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(b, a) {
+    return b - a;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function increment(n) {
+    n++;
+    return n;
+}
+function decrement(n) {
+    --n;
+    return n;
+}
+
+
+function makeInt(n) {
+    parseInt(n);
+    return parseInt(n);
+}
+function makeInt(n) {
+    return (parseInt(n, "10"));
+}
+function preserveDecimal(n) {
+    parseFloat(n);
+    return parseFloat(n);
+}
